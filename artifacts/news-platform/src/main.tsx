70c9f11e7f9faefbf6ff1798b7b49e0a1813c76e
Abhishek Jha <abhishekjha6908@gmail.com>
2026-03-29 14:20:58 +0530

    Corret api baseurl

--- artifacts/news-platform/src/main.tsx
+++ artifacts/news-platform/src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { setBaseUrl } from "@workspace/api-client-react";
 
+setBaseUrl("https://nexus-ai-intelligent-news-api-serve.vercel.app");
 createRoot(document.getElementById("root")!).render(<App />);
